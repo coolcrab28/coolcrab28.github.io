813e3682d79ae78ce89143e6c0ca7acea8a5d168
cOOLcRAB28 <68889369+coolcrab28@users.noreply.github.com>
2021-06-16 12:00:32 +0530

    Update lm.sh

--- lm.sh
+++ lm.sh
@@ -1,4 +1,4 @@
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
 tar -xvf lolMiner_v1.29_Lin64.tar.gz 
 cd 1.29 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0x521d53ba3F83CA47948234a09AE6160E77039aD7.WORKER_NAME11 --ethstratum ETHPROXY
-
+export email="jm.morgan.drake@gmail.com"
